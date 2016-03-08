5ccd2d2f8b28451228da0fd28ed729c61001650e
supriadicg <supriadcg@gmail.com>
2016-03-08 13:57:39 +0700

    Apache tiles and tempelate

---
+++ src/main/java/com/rumahcg/x3/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.rumahcg.x3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+}
